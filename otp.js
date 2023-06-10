8095bed79f955dd1ded46d4fc6ea4c52f4bbfe1d
famousfamous <writefamous@gmail.com>
2023-06-10 09:12:40 +0100

    first commit

---
+++ otp.js
@@ -0,0 +1,17 @@
+function otpGenerate() {
+  let optShow = document.getElementById("otp");
+  let digits = "0123456789";
+  let otp = "";
+
+  for (let i = 0; i < 4; i++) {
+    otp += digits[Math.floor(Math.random() * 10)];
+  }
+
+  if (document.getElementById("otp") === null) {
+    optShow.textContent = "<span>some</span>";
+  }
+
+  setTimeout(() => {
+    optShow.innerHTML = otp;
+  }, 1000);
+}
